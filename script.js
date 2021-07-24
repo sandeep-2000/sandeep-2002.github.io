83e81382decbc43ee882a15205df50535367904a
Sandeep Aloney <sandeepaloney1234@gmail.com>
2021-07-24 17:04:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["sandeep","Jaswa","jayasri","varshitha","jayanth","Ankith","Jamun","Desiree Dulce","Natasha Nice","Jordy"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
